066609521e14c0d5d4254739373068518a7e8a18
pollpeterh <pollpeter.h@gmail.com>
2018-04-14 22:36:05 -0500

    Reverting back to oringial url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,8 +103,7 @@ Rails.application.configure do
   }
 
   # Action mailer settings
-  Rails.application.routes.default_url_options[:host] = 'ornithocular.com'
-  # config.action_mailer.default_url_options[:host] = 'ornithocular.com'
+  config.action_mailer.default_url_options[:host] = 'ornithocular.com'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:                'smtp.sendgrid.net',
