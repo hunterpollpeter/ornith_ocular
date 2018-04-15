a8b00e835494b49e73496752dadd0b92588a4911
pollpeterh <pollpeter.h@gmail.com>
2018-04-14 23:10:16 -0500

    Set mailer sender

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'accounts@ornithocular.com'
+  config.mailer_sender = ENV['app93555942@heroku.com']
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
